bf71ffaf85072f8d9bf0a17b3730f783caa62d6f
Berk Güçlükol <berk.guclukol35@gmail.com>
2021-12-11 20:40:01 +0300

    minor bug fixed
    all images converted svg.
    fixed splash screen.

--- main.js
+++ main.js
@@ -1,56 +1,54 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const {app, BrowserWindow, ipcMain} = require("electron");
 const path = require("path");
 const ipc = ipcMain;
 const createWindow = () => {
-  const splash = new BrowserWindow({
-    width: 300,
-    height: 300,
-    transparent: true,
-    frame: false,
-    alwaysOnTop: true,
-  });
-  splash.setIcon("icon.png");
-  splash.loadFile("public/splash.html");
-  splash.center();
-
-  const win = new BrowserWindow({
-    width: 600, // 600
-    height: 550,
-    maximizable: false,
-    autoHideMenuBar: true,
-    resizable: false,
-    frame: false,
-    show: false,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
-  win.setIcon("icon.png");
-  win.loadFile("public/index.html");
-
-  setTimeout(function () {
-    splash.close();
-    win.show();
-  }, 5000);
-  ipc.on("app/minimize", () => {
-    win.minimize();
-  });
-  ipc.on("app/maximize", () => {
-    //
-  });
-  ipc.on("app/quit", () => {
-    app.quit();
-  });
+	const splash = new BrowserWindow({
+		width: 500,
+		height: 335,
+		transparent: true,
+		frame: false,
+		alwaysOnTop: true,
+	});
+	splash.setIcon(path.join(__dirname, "icon.png"));
+	splash.loadFile(path.join(__dirname, "public/splash.html"));
+	splash.center();
+	const win = new BrowserWindow({
+		width: 600,
+		height: 550,
+		maximizable: false,
+		autoHideMenuBar: true,
+		resizable: false,
+		frame: false,
+		show: false,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			preload: path.join(__dirname, "preload.js"),
+		},
+	});
+	win.setIcon(path.join(__dirname, "icon.png"));
+	win.loadFile(path.join(__dirname, "public/index.html"));
+	setTimeout(function () {
+		splash.close();
+		win.show();
+	}, 5000);
+    ipc.on("app/minimize", () => {
+		win.minimize();
+	});
+	ipc.on("app/maximize", () => {
+		//
+	});
+	ipc.on("app/quit", () => {
+		app.quit();
+	});
 };
 
 app.whenReady().then(() => {
-  createWindow();
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+	createWindow();
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
 });
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+	if (process.platform !== "darwin") app.quit();
 });
